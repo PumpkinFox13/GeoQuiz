728521d64040800319cdbbade2143c3f821c8ceb
PumpkinFox <apalud36@gmail.com>
2024-06-11 00:19:11 +0700

    podskazka pochti rabotaet

--- app/src/main/java/com/example/geoquiz/CheatActivity.kt
+++ app/src/main/java/com/example/geoquiz/CheatActivity.kt
@@ -1,63 +1,50 @@
 package com.example.geoquiz
 
-import android.annotation.SuppressLint
-import android.os.Build
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
+import android.app.Activity
+
+
+private const val EXTRA_ANSWER_IS_TRUE = "com.example.geoquiz.answer_is_true"
+const val EXTRA_ANSWER_SHOWN = "com.example.geoquiz.answer_shown"
 
 class CheatActivity : AppCompatActivity() {
 
-    private lateinit var quizViewModel: QuizViewModel
     private lateinit var answerTextView: TextView
     private lateinit var showAnswerButton: Button
-    private lateinit var apiLevelTextView: TextView
-    private lateinit var returnButton: Button
-    private var cheatAttempts = 0
+    private var answerIsTrue = false
 
-    @SuppressLint("StringFormatMatches")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cheat)
 
-        quizViewModel = ViewModelProvider(this).get(QuizViewModel::class.java)
+        answerIsTrue = intent.getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false)
 
         answerTextView = findViewById(R.id.answer_text_view)
         showAnswerButton = findViewById(R.id.show_answer_button)
-        apiLevelTextView = findViewById(R.id.api_level_text_view)
-        returnButton = findViewById(R.id.return_button)
-
-        apiLevelTextView.text = getString(R.string.api_level, Build.VERSION.SDK_INT)
-
         showAnswerButton.setOnClickListener {
-            if (cheatAttempts < 3) {
-                answerTextView.text = getString(R.string.correct_answer, quizViewModel.currentQuestionAnswer)
-                showAnswerButton.isEnabled = false
-                cheatAttempts++
-            } else {
-                Toast.makeText(this, getString(R.string.cheat_attempts_limit_reached), Toast.LENGTH_SHORT).show()
-                showAnswerButton.isEnabled = false
+            val answerText = if (answerIsTrue) R.string.true_button else R.string.false_button
+            answerTextView.setText(answerText)
+            setAnswerShownResult(true)
+        }
+    }
+
+    private fun setAnswerShownResult(isAnswerShown: Boolean) {
+        val data = Intent().apply {
+            putExtra(EXTRA_ANSWER_SHOWN, isAnswerShown)
+        }
+        setResult(Activity.RESULT_OK, data)
+    }
+
+    companion object {
+        fun newIntent(packageContext: Context, answerIsTrue: Boolean): Intent {
+            return Intent(packageContext, CheatActivity::class.java).apply {
+                putExtra(EXTRA_ANSWER_IS_TRUE, answerIsTrue)
             }
-        }
-
-        returnButton.setOnClickListener {
-            finish()
-        }
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putInt("cheat_attempts", cheatAttempts)
-    }
-
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
-        cheatAttempts = savedInstanceState.getInt("cheat_attempts")
-        if (cheatAttempts >= 3) {
-            showAnswerButton.isEnabled = false
         }
     }
 }
