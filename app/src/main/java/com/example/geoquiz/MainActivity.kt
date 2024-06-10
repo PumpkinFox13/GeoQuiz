911d8410dedf45b80c106ab9c0ef0662fadc05ae
PumpkinFox <apalud36@gmail.com>
2024-06-10 20:51:32 +0700

    ispravlenie oshibki otobrazheniya

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -1,27 +1,25 @@
 package com.example.geoquiz
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
 
 private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
-    private val quizViewModel: QuizViewModel by
-    lazy {
-        ViewModelProviders.of(this).get(QuizViewModel::class.java)
+    private val quizViewModel: QuizViewModel by lazy {
+        ViewModelProvider(this).get(QuizViewModel::class.java)
     }
-
 
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
@@ -32,22 +30,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         Log.d(TAG, "onCreate(Bundle?) called")
 
-        enableEdgeToEdge()
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContentView(R.layout.activity_main)
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(window.decorView) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
         trueButton = findViewById(R.id.true_button)
-        trueButton.setOnClickListener{view: View ->
+        trueButton.setOnClickListener { view: View ->
             checkAnswer(true)
         }
 
         falseButton = findViewById(R.id.false_button)
-        falseButton.setOnClickListener{view: View ->
+        falseButton.setOnClickListener { view: View ->
             checkAnswer(false)
         }
 
@@ -63,23 +61,27 @@ class MainActivity : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
-        Log.d(TAG,"onStart() called")
+        Log.d(TAG, "onStart() called")
     }
+
     override fun onResume() {
         super.onResume()
         Log.d(TAG, "onResume() called")
     }
+
     override fun onPause() {
         super.onPause()
-        Log.d(TAG,"onPause() called")
+        Log.d(TAG, "onPause() called")
     }
+
     override fun onStop() {
         super.onStop()
         Log.d(TAG, "onStop() called")
     }
+
     override fun onDestroy() {
         super.onDestroy()
-        Log.d(TAG,"onDestroy() called")
+        Log.d(TAG, "onDestroy() called")
     }
 
     private fun updateQuestion() {
@@ -87,18 +89,27 @@ class MainActivity : AppCompatActivity() {
         questionTextView.setText(questionTextResId)
         trueButton.visibility = View.VISIBLE
         falseButton.visibility = View.VISIBLE
+        if (quizViewModel.isLastQuestion()) {
+            nextButton.visibility = View.GONE
+        } else {
+            nextButton.visibility = View.VISIBLE
+        }
     }
-    private fun checkAnswer(userAnswer:Boolean) {
-        val correctAnswer = quizViewModel.currentQuestionAnswer
-        val messageResId = if (userAnswer == correctAnswer) {
+
+    @SuppressLint("StringFormatMatches")
+    private fun checkAnswer(userAnswer: Boolean) {
+        val isCorrect = quizViewModel.checkAnswer(userAnswer)
+        val messageResId = if (isCorrect) {
             R.string.correct_toast
         } else {
             R.string.incorrect_toast
         }
         trueButton.visibility = View.INVISIBLE
         falseButton.visibility = View.INVISIBLE
-        Toast.makeText(this, messageResId,
-            Toast.LENGTH_SHORT)
-            .show()
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
+
+        if (quizViewModel.isLastQuestion()) {
+            Toast.makeText(this, getString(R.string.correct_answers, quizViewModel.getCorrectAnswersCount()), Toast.LENGTH_SHORT).show()
+        }
     }
 }
