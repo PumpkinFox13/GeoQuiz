6402d032cc07f88a18d4fbf5914bb04954b80062
PumpkinFox <apalud36@gmail.com>
2024-06-10 17:11:09 +0700

    do 2.1 BUTTONS

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -55,11 +55,9 @@ class MainActivity : AppCompatActivity() {
 
         nextButton.setOnClickListener {
             currentIndex = (currentIndex + 1) % questionBank.size
-            val questionTextResId = questionBank[currentIndex].textResId
-            questionTextView.setText(questionTextResId)
+            updateQuestion()
         }
-        val questionTextResId = questionBank[currentIndex].textResId
-        questionTextView.setText(questionTextResId)
+        updateQuestion()
     }
     private fun updateQuestion() {
         val questionTextResId = questionBank[currentIndex].textResId
